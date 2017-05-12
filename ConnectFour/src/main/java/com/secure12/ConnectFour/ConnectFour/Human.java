1fff437e62534a703d1fe73fea2f283500852987
aa <trbhei@gmail.com>
2017-05-12 12:02:21 -0700

    finished Human.java with exception handling

--- ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/Human.java
+++ ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/Human.java
@@ -1,12 +1,37 @@
-import java.util.HashSet;
+package com.secure12.ConnectFour.ConnectFour;
+
+import java.util.Scanner;
 
 public class Human extends Player{
 	public Human(char playerSymbol){
 		super(playerSymbol);
 	}
 
-	public int nextColumn(){
-		// TODO nextColumn();
-		return 0;
+	public int nextColumn(String[] gameBoard){
+		int move;
+		Scanner reader = new Scanner(System.in);
+		while (true){
+			try{
+				System.out.println("Make a move[1-7]:");
+				move = reader.nextInt();
+			}
+			catch(java.util.InputMismatchException e){
+				System.out.println("Please enter an integer.");
+				continue;
+			}
+			if (move < 1 || move > 7){
+				System.out.println("Please enter an integer within [1-7].");
+			}
+			else{
+				move--;
+				if (gameBoard[move].length() >= 6){
+					System.out.printf("The column [%d] is full.\n", move+1);
+				}
+				else{
+					reader.close();
+					return move;
+				}
+			}
+		}
 	}
 }
