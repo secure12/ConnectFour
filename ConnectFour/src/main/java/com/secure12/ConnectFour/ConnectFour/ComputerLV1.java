11230139cd97cbe39d796c0e7dd6af7545e498ff
aa <trbhei@gmail.com>
2017-05-12 11:24:10 -0700

    finished ComputerLV1.java

--- ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV1.java
+++ ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV1.java
@@ -1,14 +1,21 @@
 package com.secure12.ConnectFour.ConnectFour;
 
 import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class ComputerLV1 extends Player {
 	public ComputerLV1(char playerSymbol){
 		super(playerSymbol);
 	}
-	public int nextColumn() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int nextColumn(String[] gameBoard) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < gameBoard.length; i++){
+			if (gameBoard[i].length() < 6){
+				set.add(i);
+			}
+		}
+		return ((Integer[])set.toArray())[new Random().nextInt(set.size())];
 	}
 
 }
