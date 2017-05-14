10c26d1419a4b89b32b94fff68834c1f744e85b5
aa <trbhei@gmail.com>
2017-05-14 12:22:18 -0700

    finished ComputerLV2.java

--- ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV2.java
+++ ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV2.java
@@ -8,27 +8,41 @@ public class ComputerLV2 extends ComputerLV1 {
 	}
 	
 	@Override
-	public int nextColumn(String[] gameBoard) {
-		Set<Integer> possibleColumnsSet = possibleColumns(gameBoard);
-		for (int column: possibleColumnsSet){
-			for (int direction = 0; direction < 4; direction++){
-				if (wins(gameBoard, column, direction, this)){
-					this.addDisc(gameBoard, column);
-					return column;
-				}
-			}
+	public int nextColumn(String[] gameBoard) {;
+		int column = critical(gameBoard);
+		if (column == -1){
+			column = randomColumn(possibleColumns(gameBoard));
 		}
-		for (int column: possibleColumnsSet){
-			for (int direction = 0; direction < 4; direction++){
-				if (wins(gameBoard, column, direction, this.oppo)){
-					this.addDisc(gameBoard, column);
-					return column;
-				}
-			}
-		}
-		int column = randomColumn(possibleColumnsSet);
 		this.addDisc(gameBoard, column);
 		return column;
+	}
+	
+	public int critical(String[] gameBoard){
+		Set<Integer> possibleColumnsSet = possibleColumns(gameBoard);
+		if (possibleColumnsSet.size() == 1){
+			int column = possibleColumnsSet.iterator().next();
+			this.addDisc(gameBoard, column);
+			return column;
+		}
+		else{
+			for (int column: possibleColumnsSet){
+				for (int direction = 0; direction < 4; direction++){
+					if (wins(gameBoard, column, direction, this)){
+						this.addDisc(gameBoard, column);
+						return column;
+					}
+				}
+			}
+			for (int column: possibleColumnsSet){
+				for (int direction = 0; direction < 4; direction++){
+					if (wins(gameBoard, column, direction, this.oppo)){
+						this.addDisc(gameBoard, column);
+						return column;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 	
 	private Boolean wins(String[] gameBoard, int column, int direction, Player player){
