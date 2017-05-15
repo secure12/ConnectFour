581af47627b9ce845e47488314982e0af93e0611
aa <trbhei@gmail.com>
2017-05-15 02:54:50 -0700

    changed gameBoard to Stack<Character>[] type

--- ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/Human.java
+++ ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/Human.java
@@ -1,35 +1,36 @@
 package com.secure12.ConnectFour.ConnectFour;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Human extends Player{
 	public Human(char playerSymbol){
 		super(playerSymbol);
 	}
 
-	public int nextColumn(String[] gameBoard){
-		int move;
+	public int nextColumn(Stack<Character>[] gameBoard){
+		int column;
 		Scanner reader = new Scanner(System.in);
 		while (true){
 			try{
 				System.out.println("Make a move[1-7]:");
-				move = reader.nextInt();
+				column = reader.nextInt();
 			}
 			catch(java.util.InputMismatchException e){
 				System.out.println("Please enter an integer.");
 				continue;
 			}
-			if (move < 1 || move > 7){
+			if (column < 1 || column > 7){
 				System.out.println("Please enter an integer within [1-7].");
 			}
 			else{
-				move--;
-				if (gameBoard[move].length() >= 6){
-					System.out.printf("The column [%d] is full.\n", move+1);
+				column--;
+				if (gameBoard[column].size() >= 6){
+					System.out.printf("The column [%d] is full.\n", column+1);
 				}
 				else{
 					reader.close();
-					return move;
+					return column;
 				}
 			}
 		}
