3dd2d9187dd6671e979e38b2b48f7b4fc7ce92c6
aa <trbhei@gmail.com>
2017-05-15 02:53:39 -0700

    completed ComputerLV3.java with temp heuristics function

--- ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV3.java
+++ ConnectFour/src/main/java/com/secure12/ConnectFour/ConnectFour/ComputerLV3.java
@@ -1,45 +1,100 @@
 package com.secure12.ConnectFour.ConnectFour;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 public class ComputerLV3 extends ComputerLV2 {
+	private int depth;
 	public ComputerLV3(char playerSymbol){
 		super(playerSymbol);
+		this.depth = 4;
 	}
 	
 	@Override
-	public int nextColumn(String[] gameBoard) {
-		int column = critical(gameBoard);
-		if (column != -1)
-			return column;
-		
-	}
-	
-	protected int alphaBeta(String[] gameBoard, List<Integer> validColumns, int depth, int alpha, int beta, Boolean myTurn){
-		if (depth == 0){
-			return this.heuristics_1(gameBoard);
+	public int nextColumn(Stack<Character>[] gameBoard) {
+		int col = critical(gameBoard);
+		if (col != -1)
+			return col;
+		int v = Integer.MIN_VALUE;
+		int bestColumn = 0;
+		List<Integer> validColumns = new ArrayList<Integer>();
+		for (int column: this.validColumns(gameBoard)){
+			insert(validColumns, column);
 		}
-		if (myTurn){
-			int v = -1;
-			for (int column: validColumns){
-				int index = column*6 + gameBoard[column].length();
-				this.discs.add(index);
-				gameBoard[column] += this.getPlayerSymbol();
-				if (gameBoard[column].length() == 6){
-					validColumns.remove(column);
-				}
-				v = Math.max(v, this.alphaBeta(gameBoard, validColumns, depth-1, alpha, beta, false));
-				if (!validColumns.contains(column)){
-					validColumns.
-				}
+		for (int column: validColumns){
+			int index = column*6 + gameBoard[column].size();
+			this.discs.add(index);
+			gameBoard[column].push(this.getPlayerSymbol());
+			if (gameBoard[column].size() == 6){
+				validColumns.remove(index);
+			}
+			int temp = this.alphaBeta(gameBoard, validColumns, this.depth-1, Integer.MIN_VALUE+1, Integer.MAX_VALUE, false);
+			gameBoard[column].pop();
+			this.discs.remove(index);
+			if (!validColumns.contains(column)){
+				insert(validColumns, column);
+			}
+			if (v < temp){
+				v = temp;
+				bestColumn = column;
 			}
 		}
+		return bestColumn;
 	}
 	
-	protected int heuristics_1(String[] gameBoard){
+	protected int alphaBeta(Stack<Character>[] gameBoard, List<Integer> validColumns, int depth, int alpha, int beta, Boolean myTurn){
+		if (depth == 0){
+			return this.heuristics_1(this);
+		}
+		Player player = (myTurn) ? this : this.oppo;
+		char playerSymbol = player.getPlayerSymbol();
+		int v = (myTurn) ? Integer.MIN_VALUE+1 : Integer.MAX_VALUE;
+		for (int column: validColumns){
+			int index = column*6 + gameBoard[column].size();
+			player.discs.add(index);
+			gameBoard[column].push(playerSymbol);
+			if (gameBoard[column].size() == 6){
+				validColumns.remove(column);
+			}
+			int temp = this.alphaBeta(gameBoard, validColumns, depth-1, alpha, beta, !myTurn);
+			v = (myTurn) ? Math.max(v, temp) : Math.min(v,  temp);
+			if (!validColumns.contains(column)){
+				insert(validColumns, column);
+			}
+			gameBoard[column].pop();
+			player.discs.remove(index);
+			if (myTurn){
+				alpha = Math.max(alpha, v);
+			}
+			else{
+				beta = Math.min(beta, v);
+			}
+			if (beta <= alpha){
+				break;
+			}
+		}
+		return v;
+	}
+	
+	protected void insert(List<Integer> validColumns, int column){
+		if (column < 0 || column > 6){
+			return;
+		}
+		final String ideal = "3241506";
+		int index = ideal.indexOf('0'+column);
+		for (int i = 0; i < index; i++){
+			if (validColumns.contains(ideal.charAt(i) - '0')){
+				index--;
+			}
+		}
+		validColumns.add(index, column);
+	}
+	
+	protected int heuristics_1(ComputerLV3 player){
 		int h = 0;
-		Set<Integer> discs = this.getDiscs();
+		Set<Integer> discs = player.getDiscs();
 		for (int disc: discs){
 			if (disc%6 != 5 && discs.contains(disc+1)){
 				h++;
